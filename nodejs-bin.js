e2df945e34f643537ec0c3bc6d9750957b4725c1
Ron Korving <rkorving@moneytree.jp>
2018-08-03 15:23:30 +0900

    Lint YAML files, and lint on commit

--- nodejs-bin.js
+++ nodejs-bin.js
@@ -6,6 +6,7 @@ const yaml = require('js-yaml');
 
 const filePath = path.join(__dirname, 'eslintrc', 'nodejs-bin.yml');
 
+// eslint-disable-next-line no-sync
 const config = fs.readFileSync(filePath, { encoding: 'utf8' });
 
 module.exports = yaml.safeLoad(config);
--- nodejs.js
+++ nodejs.js
@@ -6,6 +6,7 @@ const yaml = require('js-yaml');
 
 const filePath = path.join(__dirname, 'eslintrc', 'nodejs.yml');
 
+// eslint-disable-next-line no-sync
 const config = fs.readFileSync(filePath, { encoding: 'utf8' });
 
 module.exports = yaml.safeLoad(config);
