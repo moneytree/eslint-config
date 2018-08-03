05dd6a248df380025d146c74854c4a52627e98d4
Ron Korving <rkorving@moneytree.jp>
2018-08-03 12:39:35 +0900

    Added nodejs-bin rules

---
+++ nodejs-bin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+const filePath = path.join(__dirname, 'eslintrc', 'nodejs-bin.yml');
+
+const config = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+module.exports = yaml.safeLoad(config);
